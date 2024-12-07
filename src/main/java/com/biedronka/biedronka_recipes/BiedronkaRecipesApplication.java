a09cf1f15c9f60de5dc277a25f521e49cae12725
hardwak <ymhere2@gmail.com>
2024-12-07 17:00:50 +0100

    Initial commit

---
+++ src/main/java/com/biedronka/biedronka_recipes/BiedronkaRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.biedronka.biedronka_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiedronkaRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiedronkaRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biedronka/biedronka_recipes/BiedronkaRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biedronka.biedronka_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiedronkaRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
